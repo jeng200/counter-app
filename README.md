a4d6839b88eafe4cb49bdbfbecb1629ac761db11
jeng200 <jeng200@yahoo.com>
2021-10-31 17:22:01 -0400

    Add Javascript to make page interactive

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # counter-app
+Live site: https://jeng200.github.io/counter-app/
+
+I created this webpage while taking the Scrimba Frontend Developer Career Path. While creating this page I learned about Javascript: *Variables *Basic math functions *Reassigning and incrementing *Onclick event listener *Functions *InnerText *TextContent *Strings and string variables *The console log *String concatenation *Debugging online
 
 Photo Credits:
 
--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-// document.getElementById("count-el").innerText = 5
 
+// declare variables
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el");
+console.log(countEl);
+
+// initialize count as 0
 let count = 0;
 
-console.log(count);
+// listen for clicks on the increment button
+// increment count variable when button is clicked
+function increment() {
+  count = count + 1;
+  countEl.textContent = count;
+}
+// save previous entries
+function save() {
+  let countStr = count + " - "
+  saveEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0
+}
+
+
+
